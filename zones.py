d8936795d4480e04fda55f877ea0d2f891e710cd
Yvan Romé <eeymr@leeds.ac.uk>
2020-11-06 17:56:41 +0000

    In progress: crop coordinates to new zone.

--- zones.py
+++ zones.py
@@ -11,7 +11,7 @@ class Zone:
     @abc.abstractmethod
     def compact(self, cube):
         return
-
+    
     @abc.abstractmethod
     def import_coordinates_from_data_array(self, data_array_source):
         pass
@@ -24,53 +24,60 @@ class NoZone(Zone):
     
     def compact(self, data_array):
         return data_array
-
-    def import_coordinates_from_data_array(self, data_array_source):
-        return  self
+    
+    def import_coordinates(self, data_source, lon, lat, z):
+        return self
 
 
 class Box(Zone):
     
     def __init__(self, lon_min=None, lon_max=None, lat_min=None, lat_max=None, z_min=None, z_max=None,
-                 lon=None, lat=None, z=None, data_array_source=None, verbose=False):
+                 lon=None, lat=None, z=None, data_source=None, verbose=False):
         
         super(Box, self).__init__(verbose)
         
-        if data_array_source is None:
-            self.lon = lon
-            self.lat = lat
-            self.z = z
-        else:
-            self.import_coordinates_from_data_array(data_array_source)
+        self.lon, self.lat, self.z = None, None, None
+        self.lonb, self.latb, self.zb = None, None, None
+        self.lons, self.lats, self.zs = None, None, None
+        self.lon_p, self.lat_p, self.z_p = None, None, None
+        self.lonb_p, self.latb_p, self.zb_p = None, None, None
+        self.lons_p, self.lats_p, self.zs_p = None, None, None
         
-        self.lon_min = np.min(self.lon) if lon_min is None and self.lon is not None else lon_min
-        self.lon_max = np.max(self.lon) if lon_max is None and self.lon is not None else lon_max
-        self.lat_min = np.min(self.lat) if lat_min is None and self.lat is not None else lat_min
-        self.lat_max = np.max(self.lat) if lat_max is None and self.lat is not None else lat_max
-        self.z_min = np.min(self.z) if z_min is None and self.z is not None else z_min
-        self.z_max = np.max(self.z) if z_max is None and self.z is not None else z_max
+        self.import_coordinates(data_source, lon, lat, z)
+        
+        # self.lon_min = np.min(self.lon) if lon_min is None and self.lon is not None else lon_min
+        # self.lon_max = np.max(self.lon) if lon_max is None and self.lon is not None else lon_max
+        # self.lat_min = np.min(self.lat) if lat_min is None and self.lat is not None else lat_min
+        # self.lat_max = np.max(self.lat) if lat_max is None and self.lat is not None else lat_max
+        # self.z_min = np.min(self.z) if z_min is None and self.z is not None else z_min
+        # self.z_max = np.max(self.z) if z_max is None and self.z is not None else z_max
+        self.lon_min = lon_min
+        self.lon_max = lon_max
+        self.lat_min = lat_min
+        self.lat_max = lat_max
+        self.z_min = z_min
+        self.z_max = z_max
         
         # if lsm is None:
         #     self.lsm = prc.LSM().default_lsm(lon, lat, z)
         # else:
         #     self.lsm = lsm
     
-    def import_coordinates_from_data_set(self, ds):
-        try:
-            self.lon = ds.lon
-        except AttributeError:
-            self.lon = None
-        try:
-            self.lat = ds.lat
-        except AttributeError:
-            self.lat = None
-        try:
-            self.z = ds.z
-        except AttributeError:
-            self.z = None
+    def import_coordinates(self, data_source, lon, lat, z):
+
+        if data_source is not None:
+            self.lon, self.lat, self.z = data_source.lon, data_source.lat, data_source.z
+            self.lonb, self.latb, self.zb = data_source.lonb, data_source.latb, data_source.zb
+            self.lons, self.lats, self.zs = data_source.lons, data_source.lats, data_source.zs
+            self.lon_p, self.lat_p, self.z_p = data_source.lon_p, data_source.lat_p, data_source.z_p
+            self.lonb_p, self.latb_p, self.zb_p = data_source.lonb_p, data_source.latb_p, data_source.zb_p
+            self.lons_p, self.lats_p, self.zs_p = data_source.lons_p, data_source.lats_p, data_source.zs_p
+        else:
+            self.lon = lon
+            self.lat = lat
+            self.z = z
         
-        # if self.lsm is None:
-        #     self.lsm = ds.lsm
+        self.update()
     
     def get_indexes(self):
         
@@ -86,39 +93,23 @@ class Box(Zone):
     
     def compact(self, geo_da):
         # Test lon etc.
-        
-        
-        if self.lon is not None:
-            geo_da.data = geo_da.data.where(geo_da.data.longitude >= self.lon_min, drop=True) \
-                .where(geo_da.data.longitude <= self.lon_max, drop=True)
-        if self.lat is not None:
-            geo_da.data = geo_da.data.where(geo_da.data.latitude >= self.lat_min, drop=True) \
-                .where(geo_da.data.latitude <= self.lat_max, drop=True)
-        if self.z is not None:
-            geo_da.data = geo_da.data.where(geo_da.data.z >= self.z_min, drop=True) \
-                .where(geo_da.data.z <= self.z_max, drop=True)
+
+        if self.lon_min is not None:
+            geo_da.data = geo_da.data.where(geo_da.data.longitude >= self.lon_min, drop=True)
+        if self.lon_max is not None:
+            geo_da.data = geo_da.data.where(geo_da.data.longitude <= self.lon_max, drop=True)
+        if self.lat_min is not None:
+            geo_da.data = geo_da.data.where(geo_da.data.latitude >= self.lat_min, drop=True)
+        if self.lat_max is not None:
+            geo_da.data = geo_da.data.where(geo_da.data.latitude >= self.lat_max, drop=True)
+        if self.z_min is not None:
+            geo_da.data = geo_da.data.where(geo_da.data.z >= self.z_min, drop=True)
+        if self.z_max is not None:
+            geo_da.data = geo_da.data.where(geo_da.data.z <= self.z_max, drop=True)
         
         print("____ Data compacted to the zone.")
         geo_da.fit_coordinates_to_data()
         return geo_da
-    
-    def import_coordinates_from_data_array(self, data_array_source):
-        try:
-            self.lon = data_array_source.longitude.values
-        except AttributeError:
-            self.lon = None
-        try:
-            self.lat = data_array_source.latitude.values
-        except AttributeError:
-            self.lat = None
-        try:
-            self.z = data_array_source.z.values
-        except AttributeError:
-            self.z = None
-        
-        self.update()
-        
-        return self
     
     def update(self):
         
@@ -134,5 +125,14 @@ class Box(Zone):
             self.z_min = np.min(self.z)
         if self.z is not None and self.z_max is None:
             self.z_max = np.max(self.z)
-
-        
+            
+    def fit_coordinate_to_data(self):
+        if self.lon_min is not None:
+            self.lon = self.lon[np.where(self.lon >= self.lon_min)]
+            self.lon_p = self.lon_p[np.where(self.lon_p >= self.lon_min)]
+            self.lonb = self.lonb[np.where(self.lon >= self.lon)] ### not good, find something else.
+            self.lonb_p = self.lonb_p[np.where(self.lon >= self.lon)]
+            self.lons_p = self.lonb_p[1:] - self.lonb_p[0:-1]
+        if self.lon_max is not None:
+            new_lon_p = np.where(self.lon_p >= self.lon_min)
+            
