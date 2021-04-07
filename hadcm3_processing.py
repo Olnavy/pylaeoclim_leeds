763f723c9d8fc3f595d30e824b4077c9ffc11f5f
Yvan Romé <eeymr@leeds.ac.uk>
2021-04-07 12:27:56 +0100

    Added mixed layer depth to OCNMDS.

--- hadcm3_processing.py
+++ hadcm3_processing.py
@@ -503,10 +503,18 @@ class OCNMDS(HadCM3RDS):
             new_start_year=new_start_year, new_end_year=new_end_year, new_month_list=new_month_list)
 
     def ice_conc(self, zone=zones.NoZone(), mode_lon=None, value_lon=None, mode_lat=None, value_lat=None,
-            mode_t=None, value_t=None, new_start_year=None, new_end_year=None, new_month_list=None):
+                 mode_t=None, value_t=None, new_start_year=None, new_end_year=None, new_month_list=None):
         print("__ Importing ice concentration.")
         return self.get(
             xr.open_mfdataset(self.paths, combine='by_coords').iceconc_mm_uo.isel(unspecified=0).drop("unspecified"),
+            zone, mode_lon, value_lon, mode_lat, value_lat, None, None, mode_t, value_t,
+            new_start_year=new_start_year, new_end_year=new_end_year, new_month_list=new_month_list)
+
+    def mld(self, zone=zones.NoZone(), mode_lon=None, value_lon=None, mode_lat=None, value_lat=None,
+                 mode_t=None, value_t=None, new_start_year=None, new_end_year=None, new_month_list=None):
+        print("__ Importing mixed layer depth.")
+        return self.get(
+            xr.open_mfdataset(self.paths, combine='by_coords').mixLyrDpth_mm_uo.isel(unspecified=0).drop("unspecified"),
             zone, mode_lon, value_lon, mode_lat, value_lat, None, None, mode_t, value_t,
             new_start_year=new_start_year, new_end_year=new_end_year, new_month_list=new_month_list)
 
