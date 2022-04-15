5cb9b530c5f3293f027bb2032dca4eead9ae528f
Olnavy <yv.rome@gmail.com>
2022-04-15 14:56:57 +0100

    Simplified Box zone.

--- zones.py
+++ zones.py
@@ -33,26 +33,10 @@ class NoZone(Zone):
 
 class Box(Zone):
     
-    def __init__(self, lon_min=None, lon_max=None, lat_min=None, lat_max=None, z_min=None, z_max=None,
-                 lon=None, lat=None, z=None, data_source=None, verbose=False):
+    def __init__(self, lon_min=None, lon_max=None, lat_min=None, lat_max=None, z_min=None, z_max=None, verbose=False):
         
         super(Box, self).__init__(verbose)
         
-        # self.lon, self.lat, self.z = None, None, None
-        # self.lonb, self.latb, self.zb = None, None, None
-        # self.lons, self.lats, self.zs = None, None, None
-        # self.lon_p, self.lat_p, self.z_p = None, None, None
-        # self.lonb_p, self.latb_p, self.zb_p = None, None, None
-        # self.lons_p, self.lats_p, self.zs_p = None, None, None
-        
-        # self.import_coordinates(data_source, lon, lat, z)
-        
-        # self.lon_min = np.min(self.lon) if lon_min is None and self.lon is not None else lon_min
-        # self.lon_max = np.max(self.lon) if lon_max is None and self.lon is not None else lon_max
-        # self.lat_min = np.min(self.lat) if lat_min is None and self.lat is not None else lat_min
-        # self.lat_max = np.max(self.lat) if lat_max is None and self.lat is not None else lat_max
-        # self.z_min = np.min(self.z) if z_min is None and self.z is not None else z_min
-        # self.z_max = np.max(self.z) if z_max is None and self.z is not None else z_max
         self.lon_min = lon_min
         self.lon_max = lon_max
         self.lat_min = lat_min
@@ -60,23 +44,10 @@ class Box(Zone):
         self.z_min = z_min
         self.z_max = z_max
         
-        # if lsm is None:
-        #     self.lsm = prc.LSM().default_lsm(lon, lat, z)
-        # else:
-        #     self.lsm = lsm
-    
     def __repr__(self):
         return f"lon_min: {self.lon_min}, lon_max: {self.lon_max}, lat_min: {self.lat_min}, lat_max: {self.lat_max}, " \
                f"z_min: {self.z_min}, z_max: {self.z_max}\n" \
-            # f"{util.print_coordinates('lon', self.lon)}; {util.print_coordinates('lon_p', self.lon_p)}\n" \
-        # f"{util.print_coordinates('lonb', self.lonb)}; {util.print_coordinates('lonb_p', self.lonb_p)}\n" \
-        # f"{util.print_coordinates('lons', self.lons)}; {util.print_coordinates('lons_p', self.lons_p)}\n" \
-        # f"{util.print_coordinates('lat', self.lat)}; {util.print_coordinates('lat_p', self.lat_p)}\n" \
-        # f"{util.print_coordinates('latb', self.latb)}; {util.print_coordinates('latb_p', self.latb_p)}\n" \
-        # f"{util.print_coordinates('lats', self.lats)}; {util.print_coordinates('lats_p', self.lats_p)}\n" \
-        # f"{util.print_coordinates('z', self.z)}; {util.print_coordinates('z_p', self.z_p)}\n" \
-        # f"{util.print_coordinates('zb', self.zb)}; {util.print_coordinates('zb_p', self.zb_p)}\n" \
-        # f"{util.print_coordinates('zs', self.zs)}; {util.print_coordinates('zs_p', self.zs_p)}\n" \
+    
     
     def compact(self, geo_da):
         # Test lon etc.
@@ -106,35 +77,22 @@ class Box(Zone):
                 util.coordinate_to_index(geo_da.data.zb, self.z_min),
                 util.coordinate_to_index(geo_da.data.zb, self.z_max)))
 
-            
-        # if self.lon_min is not None and 'longitude' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.longitude >= self.lon_min, drop=True)
-        # if self.lon_min is not None and 'longitudeb' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.longitudeb >= self.lon_min, drop=True)
-        # if self.lon_max is not None and 'longitude' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.longitude <= self.lon_max, drop=True)
-        # if self.lon_max is not None and 'longitudeb' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.longitudeb <= self.lon_max, drop=True)
-        # if self.lat_min is not None and 'latitude' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.latitude >= self.lat_min, drop=True)
-        # if self.lat_min is not None and 'latitudeb' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.latitudeb >= self.lat_min, drop=True)
-        # if self.lat_max is not None and 'latitude' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.latitude <= self.lat_max, drop=True)
-        # if self.lat_max is not None and 'latitudeb' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.latitudeb <= self.lat_max, drop=True)
-        # if self.z_min is not None and 'z' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.z >= self.z_min, drop=True)
-        # if self.z_min is not None and 'zb' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.zb >= self.z_min, drop=True)
-        # if self.z_max is not None and 'z' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.z <= self.z_max, drop=True)
-        # if self.z_max is not None and 'zb' in geo_da.data.dims:
-        #     geo_da.data = geo_da.data.where(geo_da.data.zb <= self.z_max, drop=True)
-        
-        print("____ Data compacted to the zone.")
+            print("____ Data compacted to the zone.")
         # geo_da.fit_coordinates_to_data()
         return self.fit_coordinates_to_data(geo_da)
+    
+    def create_cycle(self, dim='2D'):
+        """
+        Create a 2D numpy array with coordinates for polygon drawing in matplotlib.
+        :param dim:
+        :return:
+        """
+        if dim=='2D':
+            return [[self.lon_min, self.lon_min, self.lon_max, self.lon_max, self.lon_min],
+                    [self.lat_min, self.lat_max, self.lat_max, self.lat_min, self.lat_min]]
+        else:
+            print("*** Not implemented yet, cycle aborted")
+            return None
     
     def fit_coordinates_to_data(self, geo_da):
         """
